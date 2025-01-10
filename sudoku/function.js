c6f4c6263ab4142758c035840f225a32214b4818
kevinapta <kevinapta99@gmail.com>
2025-01-10 22:29:32 +0800

    belum bisa clear console ketika print

--- sudoku/function.js
+++ sudoku/function.js
@@ -1,24 +1,58 @@
 const [width, height, numbox] = [9, 9, 9];
+const readline = require("node:readline");
+const { promiseHooks } = require("node:v8");
 class grid {
-    constructor(){
-        this.template = Array.from({ length: width * height }, (el, i) => i);
-        this.rows = Array.from({ length: width }, (el, i) => this.template.slice(i * 9, (i + 1) * 9));
+    constructor({width, height, numbox, template = Array.from({ length: width * height }, (el, i) => i)}){
+        this.template = template;
+        this.rows = Array.from({ length: width }, (el, i) => template.slice(i * 9, (i + 1) * 9));
         this.miniboxes = Array.from({ length: numbox }, (el, i) => {
             let base = Math.floor(i / 3) * 27 + (i % 3) * 3; // Hitung indeks awal
             return [
-                ...this.template.slice(base, base + 3), // Elemen 3 pertama
-                ...this.template.slice(base + 9, base + 12), // Elemen 3 tengah
-                ...this.template.slice(base + 18, base + 21), // Elemen 3 terakhir
+                ...template.slice(base, base + 3), // Elemen 3 pertama
+                ...template.slice(base + 9, base + 12), // Elemen 3 tengah
+                ...template.slice(base + 18, base + 21), // Elemen 3 terakhir
             ];
         });
         this.columns = Array.from({length: height}, (el, i) => {
             let temp = []
             for(let j = 0; j < width; j++){
-                temp.push(this.template[9*j+i]);
+                temp.push(template[9*j+i]);
             }
             return temp
         })
     }
 }
 
-module.exports = {grid};
+async function question(text){
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    })
+    return new Promise((resolve)=>{
+        rl.question(text, (answer) => {
+            resolve(answer);
+            rl.close();
+        })
+    })
+}
+
+async function print(grids){
+    let temp = ``
+    for(row in grids.rows){
+        for(el in grids.rows[row]){
+            if(String(grids.rows[row][el]).length < 2){
+                grids.rows[row][el] = String(grids.rows[row][el]).padStart(2, 0);
+            }
+            if(el%3==2){
+                grids.rows[row][el] += ' '
+            }
+        }
+        if(row%3==0){
+            temp += ' \n'
+        }
+        temp += grids.rows[row] + "\n";
+    }
+    console.log(temp);
+}
+
+module.exports = {grid, question, print};
--- sudoku/index.js
+++ sudoku/index.js
@@ -1,26 +1,22 @@
-const {grid} = require('./function')
-const grid1 = new grid();
+const { json } = require('stream/consumers');
+const {...func} = require('./function')
+const [width, height, numbox] = [9, 9, 9];
+// const template = Array.from({ length: width * height }, (el, i) => i);
+const grid1 = new func.grid({width: width, height: height, numbox: numbox});
 
-
-
-function print(){
-    let temp = ``
-    for(row in rows){
-        for(el in rows[row]){
-            if(String(rows[row][el]).length < 2){
-                rows[row][el] = String(rows[row][el]).padStart(2, 0);
-            }
-            if(el%3==2){
-                rows[row][el] += ' '
-            }
-        }
-        if(row%3==0){
-            temp += ' \n'
-        }
-        temp += rows[row] + "\n";
+async function main(grid = grid1){
+    // console.clear();
+    console.log(grid.template)
+    grid = new func.grid({width: width, height: height, numbox: numbox, template: grid.template})
+    func.print(grid)
+    let pos = await func.question('masukan posisi');
+    if(Number(pos)){
+        newGrid = JSON.parse(JSON.stringify(grid))
+        newGrid.template[pos] = 'XX'
+        main(newGrid)
     }
-    console.log(temp);
 }
-console.log(grid1);
-// print()
+// console.log(grid1);
+//token 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
+main();
 // console.log(columns)
